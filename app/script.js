4fbca4148b98ad234c6e934238af7086e29e848a
Oleg <o.selivancev@gmail.com>
2018-11-23 23:37:49 +0300

    hello world

--- app/script.js
+++ app/script.js
@@ -1,6 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        textText: 'Hello Vue!'
+        textText: 'Hello Vue!',
+        style1: {color: 'blue'},
+        bpdyElement: ''
+
+    },
+    methods:{
+      clickFunc: function () {
+          console.log(this.textText + ' -- clickFunc!!');
+          this.style1.color == 'blue' ? this.style1.color = 'red' : this.style1.color = 'blue';
+          this.bodyElement = 'HAHAHA'
+
+
+    }
     }
 });
