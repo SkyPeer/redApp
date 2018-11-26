2235128e38d224d76e0a945efb68ae8b86641f7f
Oleg <o.selivancev@gmail.com>
2018-11-26 19:38:53 +0300

    serverside begin

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express'),
+    helmet = require('helmet'),
+    path = require('path'),
+    compression = require('compression'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    bodyParser = require('body-parser'),
+    fs = require('fs');
+
+app.use(helmet());
+app.use(compression());
+
+app.get('/api/users/select', function(req, res, next){
+    res.sendFile('default.json', {root: __dirname});
+    res.header('Access-Control-Allow-Origin', '*');
+
+});
+app.use('public', express.static(path.join(__dirname, './public')));
+app.use('build', express.static(path.join(__dirname, './build')));
+app.use('/', (req, res, next) => {
+    // console.log('send default')
+    res.sendFile('index.html', {root: __dirname})
+});
+
+server.listen(3000, function () {
+    console.log(`redApp Start http://localhost:3000`);
+});
